6d5618577d82ebc0198889c626f136625c123d13
DJESTRIN <david.james.estrin@outlook.com>
2024-02-13 14:59:19 -0500

    added test script for testing difficulty and updated

--- test_difficulties.sh
+++ test_difficulties.sh
@@ -1,17 +1,38 @@
 #!/bin/bash
+output_directory=$1
+mkdir -p $output_directory
+
 for i in {1..10}
 do
     CURRENTEPOCTIME=`date +%s`
     RANDOMSEED=$(($CURRENTEPOCTIME + $i))
     python3 dqn_atari_stripped.py \
-        --seed $seed \
+        --seed $RANDOMSEED \
         --dropdirectory $output_directory \
-        --exp-name $agent_name \
+        --exp-name easy$RANDOMSEED \
         --difficulty 0 \
         --track \
         --capture-video \
-        --env-id $atari_game \
-        --agenttype $agent_type \
+        --env-id ALE/SpaceInvaders-v5 \
+        --agenttype vanilla \
+        --buffer-size 400000 \
+        --save-model
+
+done
+
+for i in {1..10}
+do
+    CURRENTEPOCTIME=`date +%s`
+    RANDOMSEED=$(($CURRENTEPOCTIME + $i))
+    python3 dqn_atari_stripped.py \
+        --seed $RANDOMSEED \
+        --dropdirectory $output_directory \
+        --exp-name easy$RANDOMSEED \
+        --difficulty 1 \
+        --track \
+        --capture-video \
+        --env-id ALE/SpaceInvaders-v5 \
+        --agenttype vanilla \
         --buffer-size 400000 \
         --save-model
 
