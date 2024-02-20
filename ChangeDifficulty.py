4cb61e04ea56c18a8cd5dc52692fb57394aad845
DJESTRIN <david.james.estrin@outlook.com>
2024-02-20 13:46:34 -0500

    create a difficulty class and update dqnatari

--- ChangeDifficulty.py
+++ ChangeDifficulty.py
@@ -12,7 +12,7 @@ class ChangeDifficulty():
         self.difficulty=difficulty
 
     def modify_observation(self,observation):
-        d1,d2,d3,d4=observation.shape() #Get observation dimensions
+        d1,d2,d3,d4=observation.shape #Get observation dimensions
         noise=np.random.rand(d1,d2,d3,d4) #Get an array of noise values [0,1) same shape as observation
         observation=observation+(self.difficulty*noise) #Update observation using difficulty scalar
         observation[observation>255]=255
