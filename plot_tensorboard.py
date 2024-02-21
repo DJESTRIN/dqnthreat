0ea72b5e0ca92ee3cb95211047cefcf679dc0262
DJESTRIN <david.james.estrin@outlook.com>
2024-02-21 12:44:57 -0500

    tensorboard reader again

--- plot_tensorboard.py
+++ plot_tensorboard.py
@@ -1,6 +1,8 @@
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
+import pandas as pd
 event_acc = EventAccumulator('/athena/listonlab/scratch/anp4047/difftest4/runs/easy1708461779__1708461779/events.out.tfevents.1708461792.c0009.2543542.0')
 event_acc.Reload()
 # Show all tags in the log file
 print(event_acc.Tags())
-#event_acc.Scalars('Accuracy')
+df=pd.DataFrame(event_acc.Scalars('charts/episodic_return'))
+print(df)
