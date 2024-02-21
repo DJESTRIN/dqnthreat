2004199a1406660dcaa3ab04e6afdbf445b3b880
DJESTRIN <david.james.estrin@outlook.com>
2024-02-21 16:48:16 -0500

    corrected change difficulty class which was not changing

--- plot_tensorboard.py
+++ plot_tensorboard.py
@@ -11,11 +11,14 @@ plt.figure()
 for file in tqdm.tqdm(tbfiles):
     event_acc = EventAccumulator(file)
     event_acc.Reload()
-    df=pd.DataFrame(event_acc.Scalars('charts/episodic_return'))
-    df=df.rolling(100).mean()
-    if "easy" in file:
-        plt.plot(df["step"],df["value"],'g')
-    elif "hard" in file:
-        plt.plot(df["step"],df["value"],'r')
+    try:
+        df=pd.DataFrame(event_acc.Scalars('charts/episodic_return'))
+        df=df.rolling(100).mean()
+        if "easy" in file:
+            plt.plot(df["step"],df["value"],'g')
+        elif "hard" in file:
+            plt.plot(df["step"],df["value"],'r')
+    except:
+        print(f'{file} was skipped')
 
 plt.savefig('/home/fs01/dje4001/dqnthreat/diffcomp2.jpg')
