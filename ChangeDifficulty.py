5150c5cb593d4d359ee7f173315683e7c0851e3f
DJESTRIN <david.james.estrin@outlook.com>
2024-02-22 21:28:53 -0500

    added human playable atari include imports

--- ChangeDifficulty.py
+++ ChangeDifficulty.py
@@ -22,9 +22,7 @@ class AddNoiseToGym(ObservationWrapper):
         obs=obs+(15*noise)
         obs[obs>255]=255
         obs[obs<0]=0
-        ipdb.set_trace()
-        obsreal=tuple((obs,other))
-        return obsreal
+        return (obs,other)
 
 
 class ChangeDifficulty():
