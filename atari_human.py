639e20730f2d58c99e6373bfab2af300dcad8a90
DJESTRIN <david.james.estrin@outlook.com>
2024-02-22 20:40:18 -0500

    added human playable atari include imports

--- atari_human.py
+++ atari_human.py
@@ -23,18 +23,7 @@ def make_env(env_id, seed, idx, capture_video, run_name):
         env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
     else:
         env = gym.make(env_id, render_mode="rgb_array", difficulty=0)
-    env = gym.wrappers.RecordEpisodeStatistics(env)
-    env = NoopResetEnv(env, noop_max=30)
-    env = MaxAndSkipEnv(env, skip=4)
-    env = EpisodicLifeEnv(env)
-
-    if "FIRE" in env.unwrapped.get_action_meanings():
-        env = FireResetEnv(env)
-
-    env = ClipRewardEnv(env)
-    env = gym.wrappers.ResizeObservation(env, (84, 84))
-    env = gym.wrappers.GrayScaleObservation(env)
-    env = gym.wrappers.FrameStack(env, 4)
+   
     #env = PlayableGame(env)
     env.action_space.seed(seed)
 
