3f57af7c34fec13d1e298f08f2e2240e0d8e83e8
DJESTRIN <david.james.estrin@outlook.com>
2024-02-22 20:25:25 -0500

    added human playable atari include imports

--- atari_human.py
+++ atari_human.py
@@ -1,33 +1,47 @@
+import argparse
+import os
+import random
+import time
+from distutils.util import strtobool
 import gymnasium as gym
-
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from stable_baselines3.common.atari_wrappers import (ClipRewardEnv, EpisodicLifeEnv,FireResetEnv,MaxAndSkipEnv,NoopResetEnv)
+from stable_baselines3.common.buffers import ReplayBuffer
+from torch.utils.tensorboard import SummaryWriter
+from random import randrange
+#import Record
+from ChangeDifficulty import ChangeDifficulty
 
 def make_env(env_id, seed, idx, capture_video, run_name):
-    def thunk():
-        if capture_video and idx == 0:
-            env = gym.make(env_id, render_mode="human", difficulty=1)
-            env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
-        else:
-            env = gym.make(env_id, render_mode="human", difficulty=1)
-        env = gym.wrappers.RecordEpisodeStatistics(env)
-        env = NoopResetEnv(env, noop_max=30)
-        env = MaxAndSkipEnv(env, skip=4)
-        env = EpisodicLifeEnv(env)
+    if capture_video and idx == 0:
+        env = gym.make(env_id, render_mode="human", difficulty=1)
+        env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
+    else:
+        env = gym.make(env_id, render_mode="human", difficulty=1)
+    env = gym.wrappers.RecordEpisodeStatistics(env)
+    env = NoopResetEnv(env, noop_max=30)
+    env = MaxAndSkipEnv(env, skip=4)
+    env = EpisodicLifeEnv(env)
 
-        if "FIRE" in env.unwrapped.get_action_meanings():
-            env = FireResetEnv(env)
-        
-        env = ClipRewardEnv(env)
-        env = gym.wrappers.ResizeObservation(env, (84, 84))
-        env = gym.wrappers.GrayScaleObservation(env)
-        env = gym.wrappers.FrameStack(env, 4)
-        env.action_space.seed(seed)
+    if "FIRE" in env.unwrapped.get_action_meanings():
+        env = FireResetEnv(env)
 
-        env.seed()
-        env.reset()
+    env = ClipRewardEnv(env)
+    env = gym.wrappers.ResizeObservation(env, (84, 84))
+    env = gym.wrappers.GrayScaleObservation(env)
+    env = gym.wrappers.FrameStack(env, 4)
+    env.action_space.seed(seed)
 
-        return env
+    env.seed()
+    env.reset()
+
+    return env
     
-    return thunk
+    
 
 if __name__=='__main__':
     make_env('ALE/SpaceInvaders-v5', 57, 1, False, 'test')
