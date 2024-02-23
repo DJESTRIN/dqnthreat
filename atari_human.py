39833dcd354c374805d97cad0921fc55a8aa1094
DJESTRIN <david.james.estrin@outlook.com>
2024-02-22 20:21:22 -0500

    added human playable atari

---
+++ atari_human.py
@@ -0,0 +1,33 @@
+import gymnasium as gym
+
+
+def make_env(env_id, seed, idx, capture_video, run_name):
+    def thunk():
+        if capture_video and idx == 0:
+            env = gym.make(env_id, render_mode="human", difficulty=1)
+            env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
+        else:
+            env = gym.make(env_id, render_mode="human", difficulty=1)
+        env = gym.wrappers.RecordEpisodeStatistics(env)
+        env = NoopResetEnv(env, noop_max=30)
+        env = MaxAndSkipEnv(env, skip=4)
+        env = EpisodicLifeEnv(env)
+
+        if "FIRE" in env.unwrapped.get_action_meanings():
+            env = FireResetEnv(env)
+        
+        env = ClipRewardEnv(env)
+        env = gym.wrappers.ResizeObservation(env, (84, 84))
+        env = gym.wrappers.GrayScaleObservation(env)
+        env = gym.wrappers.FrameStack(env, 4)
+        env.action_space.seed(seed)
+
+        env.seed()
+        env.reset()
+
+        return env
+    
+    return thunk
+
+
+make_env('ALE/SpaceInvaders-v5', 57, 1, False, 'test')
